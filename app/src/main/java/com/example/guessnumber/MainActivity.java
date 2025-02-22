ac147697b9c79862ebcc48ced6885834de411283
BRYAN\bryan <bryan.zihao.fu@gmail.com>
2025-02-23 11:02:19 +1300

    2 upload

--- app/src/main/java/com/example/guessnumber/MainActivity.java
+++ app/src/main/java/com/example/guessnumber/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.guessnumber;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +10,39 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    int correctNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        generateNumber();
+        check();
+    }
+
+    private void generateNumber() {
+        Random random = new Random();
+        correctNumber = random.nextInt(10) + 1;
+    }
+
+    private void check(){
+        EditText inputNumberET = findViewById(R.id.editTextNumber);
+        Button button = findViewById(R.id.button);
+
+        button.setOnClickListener(v ->{
+                    String input = inputNumberET.getText().toString();
+                    int inputNumber = Integer.parseInt(input);
+                    if (inputNumber == correctNumber) {
+
+                    }else {
+
+                    }
         });
     }
+
 }
